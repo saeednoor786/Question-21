43e8cbaf118bff7638643c9a92f1e60071e9d8ac
Saeed Noor <163897168+saeednoor786@users.noreply.github.com>
2024-05-23 22:56:03 +0500

    twentyone assignment

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+// Q:21 They think of something you could store in a TypeScript Object. Write a program that creates 
+// Objects containing these items.
+;
+let OBJ = { Name: 'Saeed', Age: 26, Profession: 'Web Developer' };
+console.log(`\n Objects Containing Information About me:\n`);
+console.log(`My name is ${OBJ.Name}`);
+console.log(`I am  ${OBJ.Age} year old.`);
+console.log(`I am a ${OBJ.Profession}`);
---
+++ main.ts
@@ -0,0 +1,15 @@
+
+// Q:21 They think of something you could store in a TypeScript Object. Write a program that creates 
+// Objects containing these items.
+
+
+interface type {Name: string, Age: Number, Profession: string};
+
+let OBJ:type = {Name:'Saeed', Age:26, Profession:'Web Developer'};
+
+console.log(`\n Objects Containing Information About me:\n`);
+
+console.log(`My name is ${OBJ.Name}`);
+console.log(`I am  ${OBJ.Age} year old.`);
+console.log(`I am a ${OBJ.Profession}`);
+
